6741ed21e0d8d1b6916bfa90c39947878040052e
sondos bakr <sondosbak81r@gmail.com>
2025-01-14 15:43:29 +0200

    Question1 & Question2

--- Assignment03OOP/Program.cs
+++ Assignment03OOP/Program.cs
@@ -1,11 +1,100 @@
 ﻿
 
+using System;
+using System.Deployment.Internal;
+
 namespace Assignment03OOP
 {
-    internal class Program
+    #region Q1
+
+    public enum SecurityPrivileges
     {
-        static void Main(string[] args)
+        Guest,
+        Developer,
+        Security,
+        DBA
+    }
+    public class Employee
+    {
+        #region Attributes
+        private char gender;
+        #endregion
+
+        #region Property
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public HiringDate HireDate;
+        public SecurityPrivileges SecurityLevel { get; set; }
+        public char Gender
         {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    Console.WriteLine("Gener must be 'M' or 'F'");
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Employee(char gender, int iD, string name, decimal salary, HiringDate hireDate, SecurityPrivileges securityLevel)
+        {
+            Gender = gender;
+            ID = iD;
+            Name = name;
+            Salary = salary;
+            HireDate = hireDate;
+            SecurityLevel = securityLevel;
+        }
+        #endregion
+
+
+        #region Method
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Gender: {Gender}, Salary: {Salary}, Hire Date: {HireDate}, Security Level: {SecurityLevel}";
+        }
+        #endregion
+    }
+    #endregion
+
+
+
+    #region Q2
+    public class HiringDate
+    {
+        #region Properties
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        #endregion
+
+        #region Constractor
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        #endregion
+
+        #region Method
+        public override string ToString()
+        {
+            return $"Day {Day}, Month {Month}, Year {Year}";
+        }
+        #endregion
+    }
+    #endregion
+    internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+            }
         }
     }
-}
+
